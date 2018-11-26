f861c204810aa068e77f4c2ffbd01573ee4da152
Vishal Manave <>
2018-11-26 15:02:48 +0530

    Adding Base64

--- api-Learning/src/test/java/rough/HittingFirstApi.java
+++ api-Learning/src/test/java/rough/HittingFirstApi.java
@@ -5,7 +5,12 @@ import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.json.JSONObject;
@@ -19,55 +24,64 @@ import io.restassured.response.Response;
 
 
 public class HittingFirstApi {
-	
-	
+
+
 	@Test
-	public void verfyApiRestAssured() {
-		
+	public void verfyApiRestAssured() throws IOException {
+
 		Response response = given().queryParam("use_aspose", "true").contentType(ContentType.JSON).body(new File (".\\src\\test\\resources\\inputJsonFiles\\SampleData.json"))
-		.post("https://api-sandbox-rex.livecareer.com/v2/documents/export").then().extract().response();
+				.post("https://api-sandbox-rex.livecareer.com/v2/documents/export").then().extract().response();
 		//System.out.println(response.asString());
-		
+
 		response.prettyPrint();
-		
-		JSONObject jsonObj = new JSONObject(response.body().asString());
-//		System.err.println(jsonObj.get("BinaryData"));
-		
+
+		//JSONObject jsonObj = new JSONObject(response.body().asString());
+		//		System.err.println(jsonObj.get("BinaryData"));
+
 		JsonPath jsp = response.jsonPath();
+
 		String binaryData = (jsp.get("BinaryData"));
-		
+
+		System.out.println("Binary data in response "+binaryData);
+
 		int numOfPages = jsp.getInt("NumOfPages");
-		
-		Assert.assertEquals(response.getStatusCode(), 200);
-		
-	
+
+		System.out.println("Number of pages in resume :"+numOfPages);
+
+		if(response.getStatusCode() == 200) {
+			FileOutputStream file = new FileOutputStream(new File("test.docx"));
+			file.write(Base64.decodeBase64(binaryData));
+			file.close();
 		}
-	
+		else
+			Assert.fail();
+	}
+
 	/*@Test
 	public void verfyApiApache() {
-		
+
 		String postUrl = "https://api-sandbox-rex.livecareer.com/v2/documents/export";
 		HttpPost postObj = new HttpPost(postUrl);
 		postObj.setHeader("accept","application/json");
 		postObj.setEntity(new StringEntity(""));
-		
-		
+
+
 		Response response = given().queryParam("use_aspose", "true").contentType(ContentType.JSON).body(new File (".\\src\\test\\resources\\inputJsonFiles\\SampleData.json"))
 		.when().post("https://api-sandbox-rex.livecareer.com/v2/documents/export").then().extract().response();
 		//response.prettyPrint();
-		
+
 		JSONObject jsonObj = new JSONObject(response.body().asString());
 //		System.err.println(jsonObj.get("BinaryData"));
-		
+
 		JsonPath jsp = response.jsonPath();
 		String binaryData = (jsp.get("BinaryData"));
-		
+
 		int numOfPages = jsp.getInt("NumOfPages");
-		
+
 		Assert.assertEquals(response.getStatusCode(), 200);
-		
-	
+
+
 		}*/
-		
-		
+
+
 }
